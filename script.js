9227a67ee8d50ce08661e2b4d33e596713664d42
Nanda Mughny Saputra <108734293+pmughny@users.noreply.github.com>
2022-07-15 08:06:41 +0700

    update penambahan script

---
+++ script.js
@@ -0,0 +1,11 @@
+var	hideScroll = window.pageYOffset;
+	window.onscroll = function() {
+		var currentScrollPos = window.pageYOffset;
+
+		if (hideScroll > window.pageYOffset) {
+			document.getElementById("navbar").style.top = "0";
+		} else {
+			document.getElementById("navbar").style.top = "-60px";
+		}
+	hideScroll = currentScrollPos;
+	}
